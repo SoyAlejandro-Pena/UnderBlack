1bc6fb0a8b50f729ec5e4cea71bb7fd59a003028
Alejandro Peña <contacto.alejandropena@gmail.com>
2021-04-26 21:32:43 -0500

    Ya casi termino

---
+++ js/javaScript.js
@@ -0,0 +1,12 @@
+$("#cuidarPrendas").on('click', () => {
+    Swal.fire('¿Cómo cuidar tus prendas?')
+})
+$("#saberTalle").on('click', () => {
+    Swal.fire('Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repudiandae ea expedita vel est nam in similique blanditiis amet temporibus, reiciendis harum voluptas vitae architecto eligendi saepe assumenda odio magni!')
+})
+$("#telas").on('click', () => {
+    Swal.fire('¿Cómo cuidar tus prendas?')
+})
+$("#servicios").on('click', () => {
+    Swal.fire('¿Cómo cuidar tus prendas? Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repudiandae ea expedita vel est nam in similique blanditiis amet temporibus, reiciendis harum voluptas vitae architecto eligendi saepe assumenda odio magni!')
+})
