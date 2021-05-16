476b0477c39ff17d12bb98cc893b4208cf4bd3c3
Alejandro Peña <contacto.alejandropena@gmail.com>
2021-05-15 19:26:54 -0500

    Ultimo commit

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,5 +1,8 @@
 $("#cuidarPrendas").on('click', () => {
-    Swal.fire('¿Cómo cuidar tus prendas?')
+  Swal.fire({
+    title: '¿Cómo cuidar tus prendas?',
+    text: 'Lavar a mano sin frotar, sumergiendo en agua jabonosa fría. Secar colgando a la sombra. No secar al sol, no escurrir, ni secar en secarropas.',
+  })
 })
 $("#saberTalle").on('click', () => {
     Swal.fire('Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repudiandae ea expedita vel est nam in similique blanditiis amet temporibus, reiciendis harum voluptas vitae architecto eligendi saepe assumenda odio magni!')
@@ -8,5 +11,8 @@ $("#telas").on('click', () => {
     Swal.fire('¿Cómo cuidar tus prendas?')
 })
 $("#servicios").on('click', () => {
-    Swal.fire('¿Cómo cuidar tus prendas? Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repudiandae ea expedita vel est nam in similique blanditiis amet temporibus, reiciendis harum voluptas vitae architecto eligendi saepe assumenda odio magni!')
+  Swal.fire({
+    title: 'Tiempos y tipos de envios',
+    text: 'Los tiempos de entrega se manejan en días hábiles y se calculan una vez que la orden haya sido pagada y abonada.',
+  })
 })
